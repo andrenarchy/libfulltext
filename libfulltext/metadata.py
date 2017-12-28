a09d9fa7b176a4b54d0c4865d85014af89bb25b7
André Gaul <andre@gaul.io>
2017-12-28 21:53:50 +0100

    metadata: simplify crossref function

--- libfulltext/metadata.py
+++ libfulltext/metadata.py
@@ -1,5 +1,4 @@
 import requests
 
 def getCrossrefMetadata(doi):
-    r = requests.get('https://api.crossref.org/v1/works/' + doi)
-    return r.json()
+    return requests.get('https://api.crossref.org/v1/works/' + doi).json()
