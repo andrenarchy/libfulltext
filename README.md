e968000b08f00f3be7f9f49efab12805b424cc70
Michael F. Herbst <info@michael-herbst.com>
2017-12-28 23:47:58 +0100

    config handler and config generator

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # libfulltext
+
+
+## Example configuration
+Put this example configuration at `~/.config/libfulltext/config.yaml`:
+```yaml
+publishers:
+  elsevier:
+    apikey: "your_api_key_here"
+  springer: {}
+```
---
+++ bin/fulltext-generate-config.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import yaml
+from libfulltext.config import DEFAULT_CONFIG_PATH
+import os
+
+
+def dump_default_config(path):
+    path = os.path.realpath(os.path.expanduser(path))
+    if os.path.exists(path):
+        raise SystemExit("Config already exists. Bailing out.")
+
+    elsevier_api_key = input("Enter your Elsevier API key: ")
+    cfg = dict()
+    cfg["publishers"] = dict()
+    cfg["publishers"]["elsevier"] = dict()
+    cfg["publishers"]["elsevier"]["api_key"] = elsevier_api_key
+
+    dirname = os.path.dirname(path)
+    os.makedirs(dirname, exist_ok=True)
+    with open(path, "w") as f:
+        yaml.safe_dump(cfg, f)
+
+
+def main():
+    path = DEFAULT_CONFIG_PATH
+    dump_default_config(path)
+    print()
+    print("Dumped default libfulltext config at " + path)
+
+
+if __name__ == "__main__":
+    main()
