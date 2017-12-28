9fb8aa4e2449caa346fe723166716300377650bc
André Gaul <andre@gaul.io>
2017-12-28 21:51:41 +0100

    springer: add basic fetcher

---
+++ libfulltext/springer.py
@@ -0,0 +1,9 @@
+import requests
+
+def getSpringerFulltext(metadata, apiKey):
+    doi = metadata['message']['DOI']
+
+    r = requests.get('https://link.springer.com/content/pdf/{0}.pdf'.format(doi) + doi, stream=True)
+    with open('/tmp/bla.pdf', 'wb') as fd:
+        for chunk in r.iter_content(chunk_size=128):
+            fd.write(chunk)
