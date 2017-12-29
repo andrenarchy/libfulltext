f4056fb3ff3bfd82186f8abc9823c69d92127d93
Michael F. Herbst <info@michael-herbst.com>
2017-12-29 23:30:49 +0100

    Properly handle errors

--- bin/get_fulltext.py
+++ bin/get_fulltext.py
@@ -5,6 +5,7 @@
 import click
 import libfulltext
 import libfulltext.config
+import select
 
 # Settings for click
 CLICK_SETTINGS = dict(help_option_names=['-h', '--help'])
@@ -20,19 +21,24 @@ def get_fulltext(config, prefixed_ids, prefixed_id_file):
     cfg = libfulltext.config.parse(config)
 
     if prefixed_ids:
-        prefixed_id_file.close()
+        # If we have IDs on the command line, we do not want
+        # to keep blocking, waiting from stdin.
+        rlist, _, _ = select.select([prefixed_id_file], [], [], 0)
+        if rlist:
+            # If an element is in the rlist, the user supplied
+            # both a stream on stdin as well as some ids on the commandline,
+            # which we do not support.
+            raise SystemExit("Either you provide prefixed IDs on STDIN, a file "
+                             "via --prefixed-id-file or alternatively directly "
+                             "on the commandline. A combination is not allowed.")
+        else:
+            # Close the input to stop the blocking stdin.
+            prefixed_id_file.close()
     else:
         prefixed_ids = prefixed_id_file.read().split("\n")
 
-    #ids = prefixed_id_file.read().split("\n")
-    #if ids and prefixed_ids:
-    #    raise SystemExit("Either you provide prefixed IDs on STDIN, a file "
-    #                     "via --prefixed-id-file or alternatively directly "
-    #                     "on the commandline. A combination is not allowed.")
-    #ids += prefixed_ids
-
     for prfid in prefixed_ids:
-        libfulltext.get_fulltext(prfid, "/tmp/libfulltext", config)
+        libfulltext.get_fulltext(prfid, "/tmp/libfulltext", cfg)
 
 if __name__ == '__main__':
     get_fulltext()
