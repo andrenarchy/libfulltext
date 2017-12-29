f20fa51349f86d7be186057c0cd918516ca7813e
Michael F. Herbst <info@michael-herbst.com>
2017-12-29 18:37:45 +0100

    Add rudimentary click interface

--- bin/get_fulltext.py
+++ bin/get_fulltext.py
@@ -2,16 +2,37 @@
 
 """get_fulltext CLI command"""
 
+import click
 import libfulltext
-from libfulltext.config import Config
+import libfulltext.config
 
-def main():
-    """Gets fulltexts for prefixed identifiers"""
-    config = Config()
-    libfulltext.get_fulltext('doi:10.1016/j.physletb.2017.11.066',
-                             '/tmp/libfulltext',
-                             config
-                            )
+# Settings for click
+CLICK_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+
+@click.command(context_settings=CLICK_SETTINGS)
+@click.option("-c", "--config", default=libfulltext.config.DEFAULT_CONFIG_PATH,
+              type=click.File())
+@click.argument("prefixed_ids", nargs=-1, default=None)
+@click.option("-f", "--prefixed-id-file", default="-", type=click.File())
+def get_fulltext(config, prefixed_ids, prefixed_id_file):
+    # Setup the config dictionary:
+    cfg = libfulltext.config.parse(config)
+
+    if prefixed_ids:
+        prefixed_id_file.close()
+    else:
+        prefixed_ids = prefixed_id_file.read().split("\n")
+
+    #ids = prefixed_id_file.read().split("\n")
+    #if ids and prefixed_ids:
+    #    raise SystemExit("Either you provide prefixed IDs on STDIN, a file "
+    #                     "via --prefixed-id-file or alternatively directly "
+    #                     "on the commandline. A combination is not allowed.")
+    #ids += prefixed_ids
+
+    for prfid in prefixed_ids:
+        libfulltext.get_fulltext(prfid, "/tmp/libfulltext", config)
 
 if __name__ == '__main__':
-    main()
+    get_fulltext()
