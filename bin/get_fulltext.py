7d27885931723f0214423e3ab5d71183320a3860
Michael F. Herbst <info@michael-herbst.com>
2017-12-29 23:41:59 +0100

    Have more useful help for click

--- bin/get_fulltext.py
+++ bin/get_fulltext.py
@@ -13,14 +13,24 @@ CLICK_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 @click.command(context_settings=CLICK_SETTINGS)
 @click.option("-c", "--config", default=libfulltext.config.DEFAULT_CONFIG_PATH,
-              type=click.File())
-@click.argument("prefixed_ids", nargs=-1, default=None)
-@click.option("-f", "--prefixed-id-file", default="-", type=click.File())
-def get_fulltext(config, prefixed_ids, prefixed_id_file):
+              type=click.File(), help="Path to the configuration file to use.")
+@click.argument("prefixed_id", nargs=-1, default=None)
+@click.option("-f", "--prefixed-id-file", default="-", type=click.File(),
+              help="File with list of prefixed document identifiers, one per line. "
+              "By default '-', i.e. stdin, is chosen.")
+def get_fulltext(config, prefixed_id, prefixed_id_file):
+    """
+    Obtain the fulltext pdfs for a number of documents,
+    which are identified by so-called prefixed document identifiers.
+
+    Examples for prefixed document identifiers:
+        doi:10.1016/j.cortex.2015.10.021
+    """
+
     # Setup the config dictionary:
     cfg = libfulltext.config.parse(config)
 
-    if prefixed_ids:
+    if prefixed_id:
         # If we have IDs on the command line, we do not want
         # to keep blocking, waiting from stdin.
         rlist, _, _ = select.select([prefixed_id_file], [], [], 0)
@@ -35,10 +45,11 @@ def get_fulltext(config, prefixed_ids, prefixed_id_file):
             # Close the input to stop the blocking stdin.
             prefixed_id_file.close()
     else:
-        prefixed_ids = prefixed_id_file.read().split("\n")
+        prefixed_id = prefixed_id_file.read().split("\n")
 
-    for prfid in prefixed_ids:
+    for prfid in prefixed_id:
         libfulltext.get_fulltext(prfid, "/tmp/libfulltext", cfg)
+
 
 if __name__ == '__main__':
     get_fulltext()
