d8c948c1dd9cb8652dec494ad80249d492333ffb
André Gaul <andre@gaul.io>
2017-12-29 12:42:13 +0100

    fulltext: explicit matching for handlers

--- bin/get_fulltext.py
+++ bin/get_fulltext.py
@@ -8,7 +8,7 @@ from libfulltext.config import Config
 def main():
     """Gets fulltexts for prefixed identifiers"""
     config = Config()
-    libfulltext.get_fulltext(config["publishers"],
+    libfulltext.get_fulltext(config,
                              'doi:10.1016/j.physletb.2017.11.066')
 
 if __name__ == '__main__':
