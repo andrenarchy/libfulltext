b1b3dd64e6d6e42dfc7969067c5973047540c449
André Gaul <andre@gaul.io>
2017-12-29 13:06:09 +0100

    fulltext: rename crossref_member_id to crossref_member

--- libfulltext/fulltext.py
+++ libfulltext/fulltext.py
@@ -11,13 +11,13 @@ def handle_doi(config, doi):
     doi = doi.lower()
 
     metadata = get_crossref_metadata(doi)
-    crossref_member_id = metadata['message']['member']
+    crossref_member = metadata['message']['member']
 
-    if crossref_member_id == '16':
+    if crossref_member == '16':
         return get_aps_fulltext(metadata)
-    elif crossref_member_id == '78':
+    elif crossref_member == '78':
         return get_elsevier_fulltext(metadata, apikey=config['publishers']['elsevier']['apikey'])
-    elif crossref_member_id == '297':
+    elif crossref_member == '297':
         return get_springer_fulltext(metadata)
 
     raise ValueError('No handler for DOI {0} (publisher {1}) found.'
