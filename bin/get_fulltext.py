64f45c0054ab7e8648ea4ade4077704e35e8974a
Paul Seyfert <pseyfert.mathphys@gmail.com>
2017-12-30 16:14:34 +0100

    review fixes to command line parsing
     - change prefixed_id from singular to plural
     - expand docstring for get_fulltext

--- bin/get_fulltext.py
+++ bin/get_fulltext.py
@@ -14,23 +14,31 @@ CLICK_SETTINGS = dict(help_option_names=['-h', '--help'])
 @click.command(context_settings=CLICK_SETTINGS)
 @click.option("-c", "--config", default=libfulltext.config.DEFAULT_CONFIG_PATH,
               type=click.File(), help="Path to the configuration file to use.")
-@click.argument("prefixed_id", nargs=-1, default=None)
+@click.argument("prefixed_ids", nargs=-1, default=None)
 @click.option("-f", "--prefixed-id-file", default="-", type=click.File(),
               help="File with list of prefixed document identifiers, one per line. "
               "By default '-', i.e. stdin, is chosen.")
-def get_fulltext(config, prefixed_id, prefixed_id_file):
+def get_fulltext(config, prefixed_ids, prefixed_id_file):
     """
     Obtain the fulltext pdfs for a number of documents,
     which are identified by so-called prefixed document identifiers.
 
     Examples for prefixed document identifiers:
         doi:10.1016/j.cortex.2015.10.021
+
+    Args:
+        config:            path to configuration file (string)
+        prefixed_ids:      list of prefixed document identifiers (list of strings)
+        prefixed_id_file:  plain text file with prefixed document identifiers (stream)
+
+    Raises:
+        SystemExit: incompatible inputs (identifiers from multiple inputs)
     """
 
     # Setup the config dictionary:
     cfg = libfulltext.config.parse(config)
 
-    if prefixed_id:
+    if prefixed_ids:
         # If we have IDs on the command line, we do not want
         # to keep blocking, waiting from stdin.
         rlist, _, _ = select.select([prefixed_id_file], [], [], 0)
@@ -45,9 +53,9 @@ def get_fulltext(config, prefixed_id, prefixed_id_file):
             # Close the input to stop the blocking stdin.
             prefixed_id_file.close()
     else:
-        prefixed_id = [line.strip() for line in prefixed_id_file.readlines()]
+        prefixed_ids = [line.strip() for line in prefixed_id_file.readlines()]
 
-    for prfid in prefixed_id:
+    for prfid in prefixed_ids:
         print("Downloading", prfid)
         libfulltext.get_fulltext(prfid, "/tmp/libfulltext", cfg)
 
