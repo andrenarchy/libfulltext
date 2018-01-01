7c31549bd7383726c72e2e8d750dc1525d73190e
André Gaul <andre@gaul.io>
2018-01-01 16:39:23 +0100

    aps,springer: add tests for exception if doi doesn't exist

--- libfulltext/doi/crossref/test_aps.py
+++ libfulltext/doi/crossref/test_aps.py
@@ -1,11 +1,23 @@
 """Unit tests for APS"""
 
+from unittest import TestCase
+
 from ...test_utils import assert_stream
 from .aps import get_aps_fulltext
 
-def test_get_aps_fulltext():
-    """Get a fulltext from APS"""
-    get_aps_fulltext(
-        '10.1103/PhysRevPhysEducRes.13.020141',
-        assert_stream('4a1b37cf8dc7699d01b744a1f6da8fbcba8e3b6d', 'fulltext.pdf')
-        )
+class GetApsFulltextTest(TestCase):
+    """Test get_aps_fulltext"""
+
+    @staticmethod
+    def test_sha1():
+        """Compare SHA1 and filename"""
+        get_aps_fulltext(
+            '10.1103/PhysRevPhysEducRes.13.020141',
+            assert_stream('4a1b37cf8dc7699d01b744a1f6da8fbcba8e3b6d', 'fulltext.pdf')
+            )
+
+    def test_non_existent_doi(self):
+        """A non-existing DOI should result in an error"""
+        with self.assertRaises(Exception) as context:
+            get_aps_fulltext('10.1103/non-existent', lambda stream, filename: None)
+        self.assertIn('Not Found', str(context.exception))
--- libfulltext/doi/crossref/test_springer.py
+++ libfulltext/doi/crossref/test_springer.py
@@ -1,11 +1,23 @@
 """Unit tests for Springer"""
 
+from unittest import TestCase
+
 from ...test_utils import assert_stream
 from .springer import get_springer_fulltext
 
-def test_get_springer_fulltext():
-    """Get a fulltext from Springer"""
-    get_springer_fulltext(
-        '10.1140/epjc/s10052-016-4338-8',
-        assert_stream('4d188155b7d395356d6f62794f41cc6d083296b0', 'fulltext.pdf')
-        )
+class GetSpringerFulltextTest(TestCase):
+    """Test get_springer_fulltext"""
+
+    @staticmethod
+    def test_sha1():
+        """Compare SHA1 and filename"""
+        get_springer_fulltext(
+            '10.1140/epjc/s10052-016-4338-8',
+            assert_stream('4d188155b7d395356d6f62794f41cc6d083296b0', 'fulltext.pdf')
+            )
+
+    def test_non_existent_doi(self):
+        """A non-existing DOI should result in an error"""
+        with self.assertRaises(Exception) as context:
+            get_springer_fulltext('10.1140/epjc/non-existent', lambda stream, filename: None)
+        self.assertIn('Not Found', str(context.exception))
