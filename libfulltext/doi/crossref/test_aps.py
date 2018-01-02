4d90a4918b51722bf576c2532562240ef1bb06c9
André Gaul <andre@gaul.io>
2018-01-02 10:59:42 +0100

    aps,springer: add no access tests

--- libfulltext/doi/crossref/test_aps.py
+++ libfulltext/doi/crossref/test_aps.py
@@ -17,6 +17,15 @@ class GetApsFulltextTest(TestCase):
             assert_stream('4a1b37cf8dc7699d01b744a1f6da8fbcba8e3b6d', 'fulltext.pdf')
             )
 
+    def test_no_access(self):
+        """No access should be detected."""
+        with self.assertRaises(Exception) as context:
+            get_aps_fulltext(
+                '10.1103/PhysRevA.96.063419',
+                lambda stream, filename: None,
+                )
+        self.assertIn('Unauthorized', str(context.exception))
+
     def test_non_existent_doi(self):
         """A non-existing DOI should result in an error"""
         with self.assertRaises(Exception) as context:
--- libfulltext/doi/crossref/test_springer.py
+++ libfulltext/doi/crossref/test_springer.py
@@ -17,6 +17,15 @@ class GetSpringerFulltextTest(TestCase):
             assert_stream('4d188155b7d395356d6f62794f41cc6d083296b0', 'fulltext.pdf')
             )
 
+    def test_no_access(self):
+        """No access should be detected."""
+        with self.assertRaises(Exception) as context:
+            get_springer_fulltext(
+                '10.1007/978-3-658-06610-9',
+                lambda stream, filename: None,
+                )
+        self.assertIn('you do not have access', str(context.exception))
+
     def test_non_existent_doi(self):
         """A non-existing DOI should result in an error"""
         with self.assertRaises(Exception) as context:
